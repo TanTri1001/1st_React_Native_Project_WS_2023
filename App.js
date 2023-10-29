e9de4eb55c9a6b4c00de7eece885969791dbbd06
Tantri <tanyaluk.tri@gmail.com>
2023-10-29 19:42:15 +0100

    create image objects with switch logic

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+const catImage = {
+    location: require("../AwesomeProject/assets/cat.jpeg"),
+    message: "bored of cat? click \"change\""}
+const guineapigImage = {
+    location: require("../AwesomeProject/assets/guineapig.jpg"),
+    message: "bored of guinea pig? click \"change\""}
+
+
 
 export default function App() {
+    const [content, setContent] = useState(catImage)
+    const onClickChangePic = () => {
+        if (content === catImage) {
+            setContent(guineapigImage)
+        } else {setContent(catImage)}
+    }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Image
+           source={content.location}
+           style={{width: 200, height: 200}}
+         />
+      <Text>{content.message}</Text>
+
+        <Button
+            title = "change"
+            onPress = {onClickChangePic}
+
+        />
       <StatusBar style="auto" />
     </View>
   );
